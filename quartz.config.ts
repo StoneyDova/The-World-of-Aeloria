49d8df2550921f8de377c8ef423ac4d9b167f1c9
StoneyDova <133552713+StoneyDova@users.noreply.github.com>
2025-10-19 17:09:48 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Paragons of Aeloria",
+    pageTitle: "Quartz 4",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
