21ea3d680aefb943bf46721a21b5859a1e24d16f
StoneyDova <jordanrolston2@gmail.com>
2025-10-22 21:48:58 -0400

    FIX: Correct baseUrl and add deployment workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "stoneydova.github.io/The-World-of-Aeloria",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
